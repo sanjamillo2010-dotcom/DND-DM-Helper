5443b2588ec7826524da97145b7e1d19aee1de4e
Sango <sanjamillo2010@gmail.com>
2026-02-24 18:55:55 +0100

    Delete DND-DM-Helper-Windows directory

--- DND-DM-Helper-Windows/divinite.cpp
+++
@@ -1,7 +0,0 @@
-#include "divinite.h"
-
-#include <iostream>
-
-void Divinite::print_divinity_info() {
-    std::cout << "Divinity: " << name << "\nAlignment: " << alignment.alig1 << " " << alignment.alig2 << "\n";
-}
